282a6069db1ab8de979c30778259e6a63ccc02fe
jiangbin <jb!@1234>
2018-11-29 00:15:43 +0800

    自定义用户登录，占时采用JDBCTokenStore

---
+++ spring-cloud-andey/spring-cloud-oauth/src/main/java/com/andey/AuthorizationServerConfig.java
@@ -0,0 +1,68 @@
+package com.andey;
+
+import com.andey.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by jiangbin on 2018/11/29.
+ */
+@Configuration
+public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    //客户端信息注入
+    @Bean("jdbcClientDetailsService")
+    public JdbcClientDetailsService getJdbcClientDetailsService() {
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    @Bean("jdbcTokenStore")
+    public JdbcTokenStore getJdbcTokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // 使用JdbcClientDetailsService客户端详情服务
+        clients.withClientDetails(getJdbcClientDetailsService());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                // refresh_token需要userDetailsService
+                .reuseRefreshTokens(false).userDetailsService(userService)
+                .tokenStore(getJdbcTokenStore());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+        oauthServer
+                // 开启/oauth/token_key验证端口无权限访问
+                .tokenKeyAccess("permitAll()")
+                // 开启/oauth/check_token验证端口认证权限访问
+                .checkTokenAccess("isAuthenticated()");
+    }
+}
--- spring-cloud-andey/spring-cloud-oauth/src/main/java/com/andey/config/oauth/WebSecurityConfig.java
+++ spring-cloud-andey/spring-cloud-oauth/src/main/java/com/andey/config/oauth/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.andey.config.oauth;
 
+import com.andey.service.UserService;
 import com.andey.service.UsernameUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,15 +17,46 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 
+    //自动注入userDetailService
+    @Autowired
+    private UserService userService;
+
+    //自动注入加密方式
+    @Bean
+    public BCryptPasswordEncoder myEncoder(){
+        return new BCryptPasswordEncoder(6);
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        // 设置userDetailsService
+        provider.setUserDetailsService(userService);
+        // 禁止隐藏用户未找到异常
+        provider.setHideUserNotFoundExceptions(false);
+        // 使用BCrypt进行密码的hash
+        provider.setPasswordEncoder(myEncoder());
+        return provider;
+    }
+
+    //用户及授权信息的管理集合
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        //先把basic校验关掉试试效果
-        http.httpBasic().disable();
-        // 配置登陆页/login并允许访问
-//        http.formLogin().loginPage("/login").permitAll()
-//        .and().authorizeRequests().anyRequest().authenticated()
+         //先把basic校验关掉试试效果
+         http.httpBasic().disable();
+         // 配置登陆页/login并允许访问
+         http.formLogin().loginPage("/login").permitAll()
+        .and().authorizeRequests().anyRequest().authenticated()
         //a不需要csrf，即允许跨域
-        http.csrf().disable();
+        .and().csrf().disable();
     }
 
 }
